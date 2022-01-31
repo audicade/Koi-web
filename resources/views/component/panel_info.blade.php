254eb1ab18e8da9e7fd2dc1d858b88eef5f7d7a9
M. Faisal Riftiarrasyid <juukyokai00@gmail.com>
2022-01-31 21:37:26 +0700

    removing index information panel

---
+++ resources/views/component/panel_info.blade.php
@@ -0,0 +1,57 @@
+<!-- Features -->
+<section id="features">
+    <div class="container">
+        <header>
+            <h2>Panel Informasi <strong>Koi Show</strong></h2>
+        </header>
+        <div class="row aln-center">
+            <div class="col-4 col-6-medium col-12-small">
+
+                <!-- Feature -->
+                    <section>
+                        <a href="#" class="image featured"><img src="{{ asset('/') }}images/pic01.jpg" alt="" /></a>
+                        <header>
+                            <a href="http://html5up.net"><h3>Rekap Data</h3></a>
+                        </header>
+                        <p>
+                            Data entry pada setiap <strong>Koi Show</strong> event dan championship.
+                        </p>
+                    </section>
+
+            </div>
+            <div class="col-4 col-6-medium col-12-small">
+
+                <!-- Feature -->
+                    <section>
+                        <a href="{{route('events-list')}}" class="image featured"><img src="{{ asset('/') }}images/pic02.jpg" alt="" /></a>
+                        <header>
+                            <h3>Events List</h3>
+                        </header>
+                        <p>
+                            Lihat dan urutkan semua <strong>event dan championship</strong> yang tercatat dalam sistem.
+                        </p>
+                    </section>
+
+            </div>
+            <div class="col-4 col-6-medium col-12-small">
+
+                <!-- Feature -->
+                    <section>
+                        <a href="#" class="image featured"><img src="{{ asset('/') }}images/pic03.jpg" alt="" /></a>
+                        <header>
+                            <h3>FAQ?</h3>
+                        </header>
+                        <p>
+                            <strong>Pertanyaan yagn sering ditanyakan</strong> tentang cara kerja web dan cara pendaftaran dalam event.
+                        </p>
+                    </section>
+
+            </div>
+            <div class="col-12">
+                <ul class="actions">
+                    <li><a href="#" class="button icon solid fa-file">Tell Me More</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
